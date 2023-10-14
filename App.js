4d5c59e1758d03598d8d52d39be66663e8f9dc7b
CA-NAVEDK <naved.khan@crossasyst.com>
2023-10-14 14:04:27 +0530

    Created a Local notification handler in app file

--- App.js
+++ App.js
@@ -1,10 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import * as Notifications from "expo-notifications";
+import { useEffect } from "react";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+      shouldShowAlert: true,
+    };
+  },
+});
 
 export default function App() {
+  useEffect(() =>{
+    const subscription = Notifications.addNotificationReceivedListener((notification)=>{
+    });
+    const subscription2 = Notifications.addNotificationResponseReceivedListener((response)=>{
+    })
+    return () =>{
+      subscription.remove();
+      subscription2.remove();
+
+      
+    }
+  },[])
+  async function scheduleNotificationHandler() {
+    try {
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: "My first local notification",
+          body: "This is the body of the notification.",
+          data: {
+            userName: "Max",
+          },
+        },
+        trigger: {
+          seconds: 3,
+        },
+      });
+    } catch (error) {
+      console.error("Failed to schedule notification:", error);
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Schedule Notification"
+        onPress={scheduleNotificationHandler}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +58,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
